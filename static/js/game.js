bf1a5f3b4b01a7c148360eb93bcceabe34109f0f
Krzysztof Kesy <krzysztof.kesy@gmail.com>
2014-09-29 16:20:16 +0200

    client animations added

--- static/js/game.js
+++ static/js/game.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// for debug purposes
+var pr = console.log.bind(console);
+
 var Game = Backbone.Model.extend({
 	urlRoot: '/games',
 
@@ -37,30 +40,21 @@ var Players = Backbone.Collection.extend({
 
 var ScoreBoard = Backbone.View.extend({
 	template: _.template(
-		'<table>' +
-			'<thead>' +
-				'<tr>' +
-					'<th></th>' +
-					'<th>Player</th>' +
-					'<th>Moves</th>' +
-					'<th>Score</th>' +
-				'</tr>' +
-			'</thead>' +
-
-			'<tbody>' +
-			'<% players.each(function(player, position) { %>' +
-				'<tr>' +
-					'<td><%- (position + 1) %></td>' +
-					'<td><%- player.id %></td>' +
-					'<td><%- player.get("moves") %></td>' +
-					'<td><%- player.get("score") %></td>' +
-				'</tr>' +
-			'<% }) %>' +
-			'</tbody>' +
-		'</table>'
+		'<li id="player_<%- player.id %>">' +
+			'<div class="avatar"><img src="http://robohash.org/<%- player.id %>"></img></div>' +
+			'<div class="name"><%- player.id %></div>' +
+			'<div class="moves"><%- player.moves %></div>' +
+			'<div class="score"><%- player.score %></div>' +
+		'</li>'
 	),
 
 	initialize: function() {
+
+		var _this = this;
+		var topHeight = 65;
+		var boardHeight;
+		var $main = $('.main');
+
 		this.listenTo(this.model.players, 'sort', this.render);
 		this.listenTo(this.model.players, 'sync', function(collection, response, options) {
 			if (response.game.ended === false) {
@@ -69,14 +63,58 @@ var ScoreBoard = Backbone.View.extend({
 		});
 
 		this.model.players.fetch();
+
+		this.playerEntryHeight = 50;
+		this.columnWidth = 400;
+		this.playersInColumn;
+		$(window).on('resize', function() {
+			boardHeight = $('body').height() - topHeight - 10;
+			_this.playersInColumn = Math.floor((boardHeight - _this.playerEntryHeight - 20) / _this.playerEntryHeight);
+			$main.css('height', boardHeight);
+			_this.render();
+		}).resize();
 	},
 
 	render: function() {
-		this.$el.html(this.template({
-			'players': this.model.players
-		}));
+		for(var i = 0, player; i < this.model.players.models.length; i++) {
+
+			player = this.model.players.models[i].attributes;
+			var $playerEntry = this.$el.find('#player_'+player.id);
+			if(!$playerEntry.get(0)) {
+				$playerEntry = $(this.template({
+					player: player
+				}));
+				this.$el.append($playerEntry);
+			}
+
+			$playerEntry.find('.moves').html(player.moves);
+			$playerEntry.find('.score').html(player.score);
+
+			var animate = {
+				top: i * this.playerEntryHeight + this.playerEntryHeight + 20,
+				left: 0
+			};
+
+			if(i + 1 > this.playersInColumn) {
+				animate.left = 600;
+				animate.top -= (this.playersInColumn + 1) * this.playerEntryHeight;
+			}
+			$playerEntry.stop(true).animate(animate, 300);
+		}
+
+		if(this.model.players.models.length < this.playersInColumn + 1) {
+			this.$el.css({
+				left: '50%',
+				'margin-left': this.columnWidth * -1 / 2
+			});
+		} else {
+			this.$el.css({
+				left: 0,
+				'margin-left': 0
+			});
+		}
 	}
-})
+});
 
 var PlayerBoard = Backbone.View.extend({
 	template: _.template(
