6e415d0ead4f85ddff4bc34db1b908cff690e890
Tomasz Żyźniewski <zyzniewski@gmail.com>
2014-10-16 16:10:23 +0200

    some comments in client

--- client.py
+++ client.py
@@ -4,23 +4,66 @@ import random
 import sys
 import time
 
+SERVER = 'localhost'
+
 if __name__ == '__main__':
-    token = sys.argv[1]
-    game = sys.argv[2]
+    token = sys.argv[1]  # your Access Token
+    game = sys.argv[2]  # 0 (development mode), 1 (duel), 2 (contest)
 
     time.sleep(random.random())
 
-    client = http.client.HTTPConnection('localhost', 8080)
+    # connect to the game server
+    client = http.client.HTTPConnection(SERVER, 8080)
     client.connect()
 
+    # block until the game starts
     client.request('GET', '/games/{}/board?token={}'.format(game, token))
+
     response = client.getresponse()
+    '''
+    {
+        "score": 0,  # obtained points
+        "moves": 5,  # available moves
+        "moved": [None, None],  # your last choice [x, y]
+        "board": [
+            [
+                {
+                    "points": 1,
+                    "direction": "up",
+                    "x": 0,
+                    "y": 0,
+                },
+                {
+                    "points": 0,
+                    "direction": "down",
+                    "x": 1,
+                    "y": 0,
+                }
+            ],
+            [
+                {
+                    "points": 5,
+                    "direction": "right",
+                    "x": 0,
+                    "y": 1,
+                },
+                {
+                    "points": 0,
+                    "direction": "left",
+                    "x": 1,
+                    "y": 1,
+                }
+            ]
+        ]
+    }
+    '''
 
     while response.status == 200:
         data = json.loads(response.read().decode())
 
         time.sleep(random.random() * 3 + 1)
 
+        # make your move and wait for a new round
         client.request(
             'POST', '/games/{}/board?token={}'.format(game, token),
             json.dumps({
